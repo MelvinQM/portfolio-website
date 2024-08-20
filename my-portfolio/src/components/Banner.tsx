3a816df6b87829f80987b0a5bea18b1d410272ad
Melvin Moes <melv5@live.nl>
2024-08-20 23:37:40 +0200

    Fixed Home Nav Link from sending the user too far

--- my-portfolio/src/components/Banner.tsx
+++ my-portfolio/src/components/Banner.tsx
@@ -6,31 +6,32 @@ import '../css/Banner.css'
 
 const Banner = () => {
   return (
-    <section className="banner-section" id="home">
-      <Container>
-        <Row>
-          <Col xs={12} md={6} xl={7}>
-              <div className='banner-text'>
-                <h1>{`Hi! I'm Melvin`}</h1>
-                <p>
-                  Hello, my name is Melvin. I'm currently in my third year at the
-                  <a className="ms-1 fw-bold" href="https://www.hva.nl/">Amsterdam University Of Applied Sciences</a>
-                  .&nbsp;In addition to my studies, I work part-time as a software developer at Chipsoft.
-                </p>
-                <p>
-                  
-                  I enjoy learning new skills in fields such as Software Development, Embedded Systems and Game Development.
-                </p>
+    <section id="home">
+      <div className="banner-section">
+        <Container>
+          <Row>
+            <Col xs={12} md={6} xl={7}>
+                <div className='banner-text'>
+                  <h1>{`Hi! I'm Melvin`}</h1>
+                  <p>
+                    Hello, my name is Melvin. I'm currently in my third year at the
+                    <a className="ms-1 fw-bold" href="https://www.hva.nl/">Amsterdam University Of Applied Sciences</a>
+                    .&nbsp;In addition to my studies, I work part-time as a software developer at Chipsoft.
+                  </p>
+                  <p>
+                    I enjoy learning new skills in fields such as Software Development, Embedded Systems and Game Development.
+                  </p>
+                </div>
+            </Col>
+            <Col xs={12} md={6} xl={5}>
+              <div className='banner-image-wrapper'>
+                  <div className='banner-background'></div> 
+                  <img className='banner-image' src={headerImg} alt="Header Img"/>
               </div>
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <div className='banner-image-wrapper'>
-                <div className='banner-background'></div> 
-                <img className='banner-image' src={headerImg} alt="Header Img"/>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </section>
   )
 }
